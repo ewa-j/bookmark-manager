0815988c0ec752408541e9d69c09f609c92fafef
ewa <ewa.jablonskaa@gmail.com>
2021-10-13 16:00:07 +0100

    step 10 refactored

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -17,4 +17,12 @@ describe Bookmark do
       expect(bookmarks).to include("https://www.udemy.com")
     end
   end
+
+  describe ".create" do
+    it "creates a new bookmark" do
+      Bookmark.create(url: "http://www.example.org")
+
+      expect(Bookmark.view).to include "http://www.example.org"
+    end
+  end
 end
